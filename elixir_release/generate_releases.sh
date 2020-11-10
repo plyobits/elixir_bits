9722fd2dc773cfec078273473c18e020a16a0b97
Jesper André Lyngesen Pedersen <jalpedersen@gmail.com>
2020-11-10 14:10:57 +0100

    generate initial releases.exs

---
+++ elixir_release/generate_releases.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+app=$1
+root_context=$2
+template=`dirname $0`/releases.exs.template
+
+cat $template \
+  | sed -e s/\$\{APP\}/$app/g \
+  | sed -e s/\$\{ROOT_CONTEXT\}/$root_context/g
+
